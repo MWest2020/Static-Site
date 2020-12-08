5f3ceb7650f91b8ce508619e3d2ba0204afc325f
MWest2020 <mwesterweel@hotmail.com>
2020-12-08 21:57:07 +0100

    updated package.json and created Procfile

--- app.js
+++ app.js
@@ -6,24 +6,45 @@ const app = express();
 const mainRoutes = require('./routes/index');
 const projectRoutes = require('./routes/projects');
 const aboutRoute = require('./routes/about');
-const errorRoutes = require('./routes/error');
 
 
 app.set('view engine', 'pug');
 
-console.log('app connected');
-
-
 app.use(mainRoutes);
 app.use(aboutRoute);
 app.use(projectRoutes);
-app.use(errorRoutes);
+
 //static', 
 app.use('/static', express.static('./public'));
 
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server is listening on port 3000`))
 
+
+// app.use((req, res, next) => {
+//     const err = new Error(`That project doesn't exist(yet). Feel free to contact us and we will make it for you!`);
+//     err.status = 404;
+//     next(err);
+//   });
+
+
+
+app.use((req, res, next) => {
+    const err = new Error(`Below is the stack. It's only interesting for developers`);
+    // You can test error codes here. 418 is a nice one.
+    err.status = 404;
+    next(err);
+  });
+  
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    if(err.status === 404){
+        res.render('page-not-found')
+    } else{
+        res.render('error');
+    };
+});
 
 
 
--- routes/projects.js
+++ routes/projects.js
@@ -1,44 +1,24 @@
 const express = require('express');
 const router = express.Router();
-// const data  = require('../data.json');
-
-// console.log('projects connected');
-
-
 const {projects} = require('../data.json');
-router.get('/projects/:id', (req, res, next) => {
-   const projectId = req.params.id;
-   const getProjects = projects.find( ({id}) => id === +projectId);
-   if (getProjects) {
-       res.render("project", { projects: projects[req.params.id] });
-   } else{
-       const err = new Error();
-       err.status = 404;
-       err.message = `The project that you requested doesn't exist!`
-       next(err);}
-   });
 
-
-
-router.get('/', (req, res,) => {
+router.get('/projects', (req, res,) => {
    
    res.redirect('/projects/0')
 })
 
 
+router.get('/projects/:id', (req, res, next) => {
+   const projectId = req.params.id;
+   const getProjects = projects.find( ({id}) => id === +projectId);
+   if (getProjects) {
+       res.render("project", { projects: projects[req.params.id] });
+   } else {
+       const err = new Error();
+       
+       next(err);}
+   });
 
-
-// router.get('/:id', (req, res, next) => {
-//    const { projects } = data
-//    if (projects[req.params.id]){
-//       res.render('project', { projects: projects[req.params.id] }) //projects[req.params.id] send the correct data to the pug  => req.params.id will reference :id
-//    } else {
-//        // otherwise throw the default error
-//        const err = new Error('Project Page Not Found');
-//        err.status = 404;
-//        next(err);
-//    }
-// })
 
 
 module.exports = router;
